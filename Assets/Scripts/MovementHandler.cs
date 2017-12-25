53f752ebda8f0a29ffaeec63a39744967092cf6e
Lionheart <hallel1851998@gmail.com>
2017-12-25 09:23:54 +0200

    initial commit

---
+++ Assets/Scripts/MovementHandler.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovementHandler : MonoBehaviour {
+
+    Text debug;
+    [SerializeField]
+    float speed;
+    float x_speed,y_speed;
+    [SerializeField]
+    float delta;
+    [SerializeField]
+    float radius;
+
+    void Update ()
+    {
+        x_speed = speed;
+        y_speed = speed;
+        //x_speed= speed * (float)Screen.width / (float)Screen.height;
+        //y_speed = speed * (float)Screen.height / (float)Screen.width;
+        Move();
+	}
+
+
+    private void Move()
+    {
+        float x = Input.acceleration.x * x_speed * Time.deltaTime;
+        float y = Input.acceleration.y * y_speed * Time.deltaTime;
+        transform.Translate(x,y,0);
+        RestrictMove(transform.position);
+    }
+    void RestrictMove(Vector3 pos)
+    {
+        if (pos.y + radius > Camera.main.orthographicSize)
+            pos.y = Camera.main.orthographicSize - radius;
+        else if (pos.y - radius < -Camera.main.orthographicSize)
+            pos.y = -Camera.main.orthographicSize + radius;
+        float width = Utils.GetScreenWidth();
+        if (pos.x + radius > width)
+            pos.x = width - radius;
+        else if (pos.x - radius < -width)
+            pos.x = -width + radius;
+        transform.position = pos;
+    }
+
+}
---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Utils {
+
+    public static float GetScreenWidth()
+    {
+        float screen_ratio = (float)Screen.width / (float)Screen.height;
+        return Camera.main.orthographicSize * screen_ratio;
+    }
+}
